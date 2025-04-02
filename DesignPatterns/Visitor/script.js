23ebaa8b664ed718a8dc3ea421760548d1621ec1
Jaydon Diaz <jaydondiaz0303@gmail.com>
2025-04-02 10:08:24 -0400

    Updated HTML to have Pattern Name, Finished Visitor Script

--- DesignPatterns/Visitor/script.js
+++ DesignPatterns/Visitor/script.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  function Employee(name, salary) {
+    this.name = name
+    this.salary = salary
+  }
+
+  Employee.prototype = {
+    getSalary: function () {
+      return this.salary
+    },
+    setSalary: function (sal) {
+      this.salary = sal
+    },
+    accept: function (visitorFunction) {
+      visitorFunction(this)
+    }
+  }
+
+  const dev = new Employee("Bob", 10000)
+  console.log(dev.getSalary());
+
+  function ExtraSalary(emp) {
+    emp.setSalary(emp.getSalary() * 2)
+  }
+
+  dev.accept(ExtraSalary)
+  console.log(dev.getSalary());
+
 })
