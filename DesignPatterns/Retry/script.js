92bd9e31d2e74349131afa4e6c9608b469ca9b86
Jaydon Diaz <jaydondiaz0303@gmail.com>
2025-04-02 10:39:51 -0400

    Finished Retry Script

--- DesignPatterns/Retry/script.js
+++ DesignPatterns/Retry/script.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  function retryOperation() {
+    let currentTry = 0;
+
+    while (true) {
+      try {
+        externalServiceCall()
+        console.log("Succeeded!");
+        break;
+      } catch (error) {
+        currentTry++
+        console.log(`Failed attempt ${currentTry}`);
+
+        if (currentTry >= 3) {
+          console.log("Retry maximum reached. Exiting.")
+          break;
+        }
+      }
+    }
+  }
+
+  function externalServiceCall() {
+    throw "Failure"
+  }
+
+  retryOperation()
 })
